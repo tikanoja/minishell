4af4eb390bf70375f79c38f2d58d9ea7f266179a
Jaurasma <jaurasmaa@gmail.com>
2023-04-07 20:29:32 +0300

    . and .. most likely not working

--- cd.c
+++ cd.c
@@ -25,131 +25,129 @@
 #include <limits.h>
 char *ft_strcat(char *dest, const char *src)
 {
-    // Save a pointer to the start of dest
-    char *result; 
+	char *result; 
 	
 	result = dest;
-    while (*dest != '\0')
-        dest++;
-    while (*src != '\0')
+	while (*dest != '\0')
+		dest++;
+	while (*src != '\0')
 	{
-        *dest = *src;
-        dest++;
-        src++;
-    }
-    *dest = '\0';
-    return (result);
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (result);
 }
 char *ft_realpath(const char *path, char *resolved_path)
 {
-    char cwd[PATH_MAX];
-    char *p;
+	char cwd[PATH_MAX];
+	char *p;
 
-    if (!path || !resolved_path)
+	if (!path || !resolved_path)
 	{
-        //errno = EINVAL;
-        return (NULL);
-    }
-    if (path[0] == '/')
+		//errno = EINVAL;
+		return (NULL);
+	}
+	if (path[0] == '/')
 	{ // absolute path
-    	ft_strlcpy(resolved_path, path, PATH_MAX);
-        resolved_path[PATH_MAX - 1] = '\0';
-    }
+		ft_strlcpy(resolved_path, path, PATH_MAX);
+		resolved_path[PATH_MAX - 1] = '\0';
+	}
 	else
 	{ // relative path
-        if (getcwd(cwd, sizeof(cwd)) == NULL)
-            return (NULL);
-        ft_strlcpy(resolved_path, cwd, PATH_MAX);
-        resolved_path[PATH_MAX - 1] = '\0';
-        ft_strlcat(resolved_path, "/", PATH_MAX - ft_strlen(resolved_path) - 1);
-        ft_strlcat(resolved_path, path, PATH_MAX - ft_strlen(resolved_path) - 1);
-    }
-    while ((p = ft_strnstr(resolved_path, "/./", ft_strlen(resolved_path))) != NULL)
-        ft_memmove(p + 1, p + 3, ft_strlen(p + 3) + 1);
-    while ((p = ft_strnstr(resolved_path, "/../", ft_strlen(resolved_path))) != NULL) 
+		if (getcwd(cwd, sizeof(cwd)) == NULL)
+			return (NULL);
+		ft_strlcpy(resolved_path, cwd, PATH_MAX);
+		resolved_path[PATH_MAX - 1] = '\0';
+		ft_strlcat(resolved_path, "/", PATH_MAX - ft_strlen(resolved_path) - 1);
+		ft_strlcat(resolved_path, path, PATH_MAX - ft_strlen(resolved_path) - 1);
+	}
+	while ((p = ft_strnstr(resolved_path, "/./", ft_strlen(resolved_path))) != NULL)
+		ft_memmove(p + 1, p + 3, ft_strlen(p + 3) + 1);
+	while ((p = ft_strnstr(resolved_path, "/../", ft_strlen(resolved_path))) != NULL) 
 	{
-        if (p == resolved_path)
+		if (p == resolved_path)
 		{
-            //errno = EINVAL;
-            return (NULL);
-        }
-        char *prev_slash = ft_strrchr(resolved_path, '/') - 1;
-    	ft_memmove(prev_slash, p + 3, ft_strlen(p + 3) + 1);
-    }
-    return (resolved_path);
+			//errno = EINVAL;
+			return (NULL);
+		}
+		char *prev_slash = ft_strrchr(resolved_path, '/') - 1;
+		ft_memmove(prev_slash, p + 3, ft_strlen(p + 3) + 1);
+	}
+	return (resolved_path);
 }
 
 int ft_cd(const char *user_path, int argc) 
 {
-    char *path;
-    char *oldpwd;
-    char cwd[1024]; //current working directory
+	char *path;
+	char *oldpwd;
+	char cwd[1024]; //current working directory
 	char *last_slash;
 
-    path = (char *)malloc(sizeof(char) * (ft_strlen(user_path) + 1));
-    if (!path)
+	path = (char *)malloc(sizeof(char) * (ft_strlen(user_path) + 1));
+	if (!path)
 	{
-        perror("malloc failed");
-        return (-1);
-    }
-    ft_strlcpy(path, user_path, ft_strlen(user_path));
-    if (argc == 0 || *path == '~')
-        path = getenv("HOME");
+		perror("malloc failed");
+		return (-1);
+	}
+	ft_strlcpy(path, user_path, ft_strlen(user_path));
+	if (argc == 0 || *path == '~')
+		path = getenv("HOME");
 	else if (*path == '.')
 	{
-        if (*(path + 1) == '/')
+		if (*(path + 1) == '/')
 		{
-            getcwd(cwd, sizeof(cwd));
-            ft_strcat(cwd, "/");
-            ft_strcat(cwd, path);
-            path = ft_realpath(cwd, NULL);
-        }
+			getcwd(cwd, sizeof(cwd));
+			ft_strcat(cwd, "/");
+			ft_strcat(cwd, path);
+			path = ft_realpath(cwd, NULL);
+		}
 		else if (*(path + 1) == '\0')
-            path = getenv("PWD");
+			path = getenv("PWD");
 		else if (*(path + 1) == '.' && *(path + 2) == '/') //näitä pitää varmaan vähän rukata
 		{
-            getcwd(cwd, sizeof(cwd));
-            last_slash = ft_strrchr(cwd, '/');
-            *last_slash = '\0';
-            ft_strcat(cwd, "/");
-            ft_strcat(cwd, path + 2);
-            path = ft_realpath(cwd, NULL);
-        }
-    }
+			getcwd(cwd, sizeof(cwd));
+			last_slash = ft_strrchr(cwd, '/');
+			*last_slash = '\0';
+			ft_strcat(cwd, "/");
+			ft_strcat(cwd, path + 2);
+			path = ft_realpath(cwd, NULL);
+		}
+	}
 	else if (ft_strncmp(user_path, "$OLDPWD", 7) == 0 || *user_path == '-')
-        path = getenv("OLDPWD");
-    oldpwd = getenv("PWD");
-    if(chdir(path) != 0)
+		path = getenv("OLDPWD");
+	oldpwd = getenv("PWD");
+	if(chdir(path) != 0)
 	{
-        perror(path);
-        return -1;
-    }
-    setenv("OLDPWD", oldpwd, 1);
-    setenv("PWD", getcwd(cwd, sizeof(cwd)), 1);
-	//free(path);
-    return (0);
+		perror(path);
+		return -1;
+	}
+	setenv("OLDPWD", oldpwd, 1);
+	setenv("PWD", getcwd(cwd, sizeof(cwd)), 1);
+	return (0);
 }
 
-int main(int argc, char **argv, char **envp) {
-    char path[PATH_MAX];
-    char resolved_path[PATH_MAX];
-    printf("PATH_MAX = %d\n", PATH_MAX);
+/*int main(int argc, char **argv, char **envp) {
+	char path[PATH_MAX];
+	char resolved_path[PATH_MAX];
+	printf("PATH_MAX = %d\n", PATH_MAX);
 
-    // Test my_realpath() function
-    printf("Enter a path to resolve: ");
-    scanf("%s", path);
-    if (ft_realpath(path, resolved_path) == NULL) {
-        perror("my_realpath() failed");
-        exit(EXIT_FAILURE);
-    }
-    printf("Resolved path: %s\n", resolved_path);
+	// Test my_realpath() function
+	printf("Enter a path to resolve: ");
+	scanf("%s", path);
+	if (ft_realpath(path, resolved_path) == NULL) {
+		perror("my_realpath() failed");
+		exit(EXIT_FAILURE);
+	}
+	printf("Resolved path: %s\n", resolved_path);
 
-    // Test ft_cd() function
-    char *home_dir = getenv("OLDPWD");
-    if (ft_cd(home_dir, 0) != 0) {
-        perror("ft_cd() failed");
-        exit(EXIT_FAILURE);
-    }
-    printf("Current working directory after cd to OLDPWD: %s\n", getcwd(NULL, 0));
-    return 0;
-}
+	// Test ft_cd() function
+	char *home_dir = getenv("OLDPWD");
+	if (ft_cd(".", 1) != 0) {
+		perror("ft_cd() failed");
+		exit(EXIT_FAILURE);
+	}
+	printf("Current working directory after cd to OLDPWD: %s\n", getcwd(NULL, 0));
+	return 0;
+}*/
