487f3105189ec53f971f8071de5708991302cf8b
Jaurasma <jaurasmaa@gmail.com>
2023-04-13 12:30:38 +0300

    sultautetaan osaksi jarjesttelmaa

--- minishell.h
+++ minishell.h
@@ -99,5 +99,8 @@ void malloc_env_copy(char ***envcpy, const char **envp, int rows, int i);
 void get_env_copy(char ***envcpy, const char **envp);
 int main (int argc, char **argv, const char **envp);
 
+//builtin utils
+char *ft_strcat(char *dest, const char *src);
+
 
 #endif
