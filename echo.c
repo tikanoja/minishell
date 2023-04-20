e1e186bf6ede68fe81cd21659c65b54dbbd48d50
Joonatan Aurasmaa <jaurasma@c2r3p9.hive.fi>
2023-04-20 14:33:34 +0300

    yes sitr echo is better

--- echo.c
+++ echo.c
@@ -5,6 +5,8 @@ int n_definer(char *arg)
 	int	j;
 
 	j = 1;
+	if (!arg)
+		return (0);
 	if (ft_strncmp(arg, "-n", 2) == 0)
     {
         if (arg[2] != 'n' && (is_it_whitespace(arg[2]) == 1 || arg[2] == '\0'))
@@ -21,6 +23,20 @@ int n_definer(char *arg)
 	return (0);
 }
 
+int one_n_checker(t_list *echo)
+{
+	if (echo->argc < 1) //if (echo_argc_check(echo) == 1) return(0); sellane mika tsekkaa argc ja sen et onks eka arg -n tai -nnnn ta jotaa
+		return(printf("\n"));
+	if(echo->argc == 1)
+	{
+		if (n_definer(echo->args[0]) == 1)
+			return (1);
+		else
+			return (0);
+	}
+	return (0);
+}
+
 int ft_echo(t_list *echo)
 {
 	int i;
@@ -29,10 +45,14 @@ int ft_echo(t_list *echo)
 
 	n_flag = 0;
 	i = 0;
-	if (echo->argc < 1) //if (echo_argc_check(echo) == 1) return(0); sellane mika tsekkaa argc ja sen et onks eka arg -n tai -nnnn ta jotaa
-		printf("\n");
+	if (one_n_checker(echo) == 1)
+		return (0);
+	// printf("nflag = %d && argc = %d\n", n_flag, echo->argc);
 	while(n_definer(echo->args[n_flag]) == 1)
 		n_flag++;
+
+	if (echo->argc <= n_flag)
+		return (0);	
 	i = n_flag;
 	string = malloc(sizeof(char) * (echo->argc - n_flag));
 	ft_strcat(string, echo->args[i]);
