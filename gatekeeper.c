3d1a92510d874dec9b28c5782a01f90a7ce2f8b3
Joonatan Aurasmaa <jaurasma@c2r3p9.hive.fi>
2023-04-26 16:29:53 +0300

    now it works a lot better, there is someting deeply weird going on if for example try to echo /Users/jaurasma/minishell-pull-pull, but I dont think its this little thingy witch causes it

--- gatekeeper.c
+++ gatekeeper.c
@@ -44,6 +44,65 @@ int check_dollar_end(char c)
 	return (0);
 }
 
+
+size_t	ft_strnlen(const char *s, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] != '\0' && i <= n)
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strndup(const char *s1, size_t n)
+{
+	char	*dup;
+	size_t		len;
+	size_t		i;
+
+	i = 0;
+	len = ft_strnlen((char *)s1, n);
+	dup = (char *)malloc(sizeof(char) * len + 1);
+	if (dup == NULL)
+		return (NULL);
+	while (i < len || i <=n)
+	{
+		dup[i] = s1[i];
+		i++;
+	}
+	dup[i] = '\0';
+	return (dup);
+}
+
+int is_valid_env_char(char c)
+{
+    if (c >= 'A' && c <= 'Z')
+        return 1;
+    if (c >= 'a' && c <= 'z')
+        return 1;
+    if (c >= '0' && c <= '9')
+        return 1;
+    if (c == '_')
+        return 1;
+    return 0;
+}
+
+int get_env_len(char *str)
+{
+    int len = 0;
+    int i = 1; // start from 1 to skip the $ character
+
+    while (str[i] != '\0' && is_valid_env_char(str[i])) {
+        len++;
+        i++;
+    }
+
+    return len;
+}
+
 void check_value_for_dollar(t_list *current)
 {
 	int flag;
@@ -57,94 +116,97 @@ void check_value_for_dollar(t_list *current)
 	i = 0;
 	env = malloc(1);
 	new_value = malloc(1);
-	if(check_for_dollar(current->value) == 1)
+	if (check_for_dollar(current->value) == 1)
 	{
-		while(i < len)
+		while (i < len)
 		{
-			if(current->value[i] == '\'' && flag == 0)
+			if (current->value[i] == '\'' && flag == 0)
 				flag = 1;
-			else if(current->value[i] == '\'' && flag == 1)
+			else if (current->value[i] == '\'' && flag == 1)
 				flag = 0;
-			if(current->value[i] == '$' && flag == 0)
+			if (current->value[i] == '$' && flag == 0)
 			{
 				i++;
-				while (check_dollar_end(current->value[i]) == 0)
-				{
-					env = char_join(env, current->value[i]);
-					printf("char is loop is %c\nand env is %s\n",current->value[i], env);
-					i++;
-				}
-				env = getenv(env);
-				if(env)
-				{
-					printf("this happens twice");
-					new_value = ft_strjoin(new_value, env);
-				}
-				printf("new value at point %d is %s and char is %c\n", i, new_value,current->value[i]);
-				//ft_bzero(env, ft_strlen(env));
+				env = ft_strndup(current->value + i, get_env_len(current->value + i));
+				if (getenv(env))
+					new_value = ft_strjoin(new_value, getenv(env));
+				i += ft_strlen(env);
+				free(env);
 			}
-			else 
+			else
 			{
 				new_value = char_join(new_value, current->value[i]);
 				i++;
 			}
 		}
+		//new_value[i] = '\0';
+		free(current->value);
 		current->value = new_value;
+	}
+	else
+	{
+		free(env);
+		free(new_value);
 	}
 }
 
 void check_args_for_dollar(t_list *current)
 {
-	int flag;
-	int len;
-	int i;
-	int j;
-	char *env;
-	char *new_value;
+    int flag;
+    int len;
+    int i;
+    int j;
+    char *env;
+    char *new_value;
 
-	flag = 0;
-	i = 0;
-	j = 0;
-	env = malloc(1);
-	new_value = malloc(1);
-	while(current->argc > j)
-	{
-		i = 0;
-		printf("we in args");
-		len = ft_strlen(current->args[j]);
-		if(check_for_dollar(current->args[j]) == 1)
-		{
-			while(i < len)
-			{
-				if(current->args[j][i] == '\'' && flag == 0)
-					flag = 1;
-				else if(current->args[j][i] == '\'' && flag == 1)
-					flag = 0;
-				if(current->args[j][i] == '$' && flag == 0)
-				{
-					i++;
-					while (check_dollar_end(current->args[j][i]) == 0)
-					{
-						env = char_join(env, current->args[j][i]);
-						i++;
-					}
-					//i--;
-					env = getenv(env);
-					if(env)
-						new_value = ft_strjoin(new_value, env);
-					//ft_bzero(env, ft_strlen(env));
-				}
-				else
-				{
-					new_value = char_join(new_value, current->args[j][i]);
-					i++;
-				}
-			}
+    j = 0;
+    while (j < current->argc)
+    {
+        flag = 0;
+        i = 0;
+        env = malloc(1);
+        new_value = malloc(1);
+        len = ft_strlen(current->args[j]);
+        if (check_for_dollar(current->args[j]) == 1)
+        {
+            while (i < len)
+            {
+				//printf("new_val %s\n", new_value);
+                if (current->args[j][i] == '\'' && flag == 0)
+                    flag = 1;
+                else if (current->args[j][i] == '\'' && flag == 1)
+                    flag = 0;
+                if (current->args[j][i] == '$' && flag == 0)
+                {
+                    i++;
+                    env = ft_strndup(current->args[j] + i, get_env_len(current->args[j] + i));
+					if(getenv(env))
+						new_value = ft_strjoin(new_value, getenv(env));
+					i += ft_strlen(env);
+                    free(env);
+                }
+                else
+                {
+                    new_value = char_join(new_value, current->args[j][i]);
+                    i++;
+                }
+            }
+            free(current->args[j]);
+			printf("\nthe new_val is %s\n", new_value);
+            current->args[j] = new_value;
+			printf("\nthe args is %s\n", current->args[j]);
+        }
+        else
+        {
+            new_value = ft_strdup(current->args[j]);
 			current->args[j] = new_value;
-		}
-		j++;
-	}
+			//free(new_value);
+        }
+        j++;
+    }
 }
+
+
 
 void expand_envs(t_list *head)
 {
