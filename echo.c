2753d2900f387ac60de517c3d5ea0d5e2488022f
Joonatan Aurasmaa <jaurasma@c2r3p9.hive.fi>
2023-04-27 15:13:17 +0300

    echo small fix to n parsing

--- echo.c
+++ echo.c
@@ -1,31 +1,55 @@
 #include "minishell.h"
 
+// int n_definer(char *arg)
+// {
+// 	int	j;
+
+// 	j = 1;
+// 	if (!arg)
+// 		return (0);
+// 	if (ft_strncmp(arg, "-n", 2) == 0)
+//     {
+//         if (arg[2] != 'n' && (is_it_whitespace(arg[2]) == 1 || arg[2] == '\0'))
+//             return (1);
+//         while (arg[j] == 'n')
+//         {
+//             j++;
+//             if (is_it_whitespace(arg[j]) == 1 || arg[j] == '\0')
+// 				return (1);
+// 			if (arg[j] != 'n')
+// 				return (0);
+//         }
+//     }
+// 	return (0);
+// }
 int n_definer(char *arg)
 {
-	int	j;
+    int j = 1;
+    int seen_n = 0;
 
-	j = 1;
-	if (!arg)
-		return (0);
-	if (ft_strncmp(arg, "-n", 2) == 0)
+    if (!arg)
+        return (0);
+    if (ft_strncmp(arg, "-n", 2) == 0)
     {
-        if (arg[2] != 'n' && (is_it_whitespace(arg[2]) == 1 || arg[2] == '\0'))
+        if (arg[2] != 'n' &&  arg[2] == '\0')
             return (1);
         while (arg[j] == 'n')
         {
             j++;
-            if (is_it_whitespace(arg[j]) == 1 || arg[j] == '\0')
-				return (1);
-			if (arg[j] != 'n')
-				return (0);
+            if (arg[j] == '\0')
+                return (1);
+            if (arg[j] != 'n')
+                seen_n = 1;
+            else if (seen_n)
+                return (0);
         }
     }
-	return (0);
+    return (0);
 }
 
 int one_n_checker(t_list *echo)
 {
-	if (echo->argc < 1) //if (echo_argc_check(echo) == 1) return(0); sellane mika tsekkaa argc ja sen et onks eka arg -n tai -nnnn ta jotaa
+	if (echo->argc < 1)
 		return(printf("\n"));
 	if(echo->argc == 1)
 	{
