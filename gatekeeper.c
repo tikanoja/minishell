61768c4f30aa15dde1c19fb5cc1cb018366bf9cc
Joonatan Aurasmaa <jaurasma@c2r3p9.hive.fi>
2023-04-27 15:17:08 +0300

    fixing the norm

--- gatekeeper.c
+++ gatekeeper.c
@@ -81,29 +81,29 @@ char	*ft_strndup(const char *string, size_t n)
 
 int is_valid_env_char(char c)
 {
-    if (c >= 'A' && c <= 'Z')
-        return (1);
-    if (c >= 'a' && c <= 'z')
-        return (1);
-    if (c >= '0' && c <= '9')
-        return (1);
-    if (c == '_')
-        return (1);
-    return (0);
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	if (c >= '0' && c <= '9')
+		return (1);
+	if (c == '_')
+		return (1);
+	return (0);
 }
 
 int get_env_len(char *str)
 {
-    int len;
-    int i;
+	int len;
+	int i;
 	
 	len = 0;
 	i = 1; // start from 1 to skip the $ character
-    while (str[i] != '\0' && is_valid_env_char(str[i])) {
-        len++;
-        i++;
-    }
-    return (len);
+	while (str[i] != '\0' && is_valid_env_char(str[i])) {
+		len++;
+		i++;
+	}
+	return (len);
 }
 
 void check_value_for_dollar(t_list *current)
@@ -155,99 +155,99 @@ void check_value_for_dollar(t_list *current)
 
 void check_args_for_dollar(t_list *current)
 {
-    int flag;
-    int len;
-    int i;
-    int j;
-    char *env;
-    char *new_value;
+	int flag;
+	int len;
+	int i;
+	int j;
+	char *env;
+	char *new_value;
 
-    j = 0;
-    while (j < current->argc)
-    {
-        flag = 0;
-        i = 0;
-        env = malloc(1);
-        new_value = malloc(1);
-        len = ft_strlen(current->args[j]);
-        if (check_for_dollar(current->args[j]) == 1)
-        {
-            while (i < len)
-            {
+	j = 0;
+	while (j < current->argc)
+	{
+		flag = 0;
+		i = 0;
+		env = malloc(1);
+		new_value = malloc(1);
+		len = ft_strlen(current->args[j]);
+		if (check_for_dollar(current->args[j]) == 1)
+		{
+			while (i < len)
+			{
 				//printf("new_val %s\n", new_value);
-                if (current->args[j][i] == '\'' && flag == 0)
-                    flag = 1;
-                else if (current->args[j][i] == '\'' && flag == 1)
-                    flag = 0;
-                if (current->args[j][i] == '$' && flag == 0)
-                {
-                    i++;
-                    env = ft_strndup(current->args[j] + i, get_env_len(current->args[j] + i));
+				if (current->args[j][i] == '\'' && flag == 0)
+					flag = 1;
+				else if (current->args[j][i] == '\'' && flag == 1)
+					flag = 0;
+				if (current->args[j][i] == '$' && flag == 0)
+				{
+					i++;
+					env = ft_strndup(current->args[j] + i, get_env_len(current->args[j] + i));
 					if(getenv(env))
 						new_value = ft_strjoin(new_value, getenv(env));
 					i += ft_strlen(env);
-                    free(env);
-                }
-                else
-                {
-                    new_value = char_join(new_value, current->args[j][i]);
-                    i++;
-                }
-            }
-            free(current->args[j]);
+					free(env);
+				}
+				else
+				{
+					new_value = char_join(new_value, current->args[j][i]);
+					i++;
+				}
+			}
+			free(current->args[j]);
 			printf("\nthe new_val is %s\n", new_value);
-            current->args[j] = new_value;
+			current->args[j] = new_value;
 			printf("\nthe args is %s\n", current->args[j]);
-        }
-        else
-        {
-            new_value = ft_strdup(current->args[j]);
+		}
+		else
+		{
+			new_value = ft_strdup(current->args[j]);
 			current->args[j] = new_value;
 			//free(new_value);
-        }
-        j++;
-    }
+		}
+		j++;
+	}
 }
 
 
 char *parse_quotes(char *str)
 {
-    int i = 0;
-    char *new_string;
-    char quote;
+	int i = 0;
+	char *new_string;
+	char quote;
 
 	new_string = NULL;
 	quote = '\0';
-    if (str == NULL)
-        return NULL;
-    new_string = (char*) malloc(ft_strlen(str) + 1);
-    if (new_string == NULL)
-        return NULL;
-    while (str[i])
+	if (str == NULL)
+		return NULL;
+	new_string = (char*)malloc(ft_strlen(str) + 1);
+	if (new_string == NULL)
+		return NULL;
+	while (str[i])
 	{
-        if (str[i] == '\'' && !quote)
-            quote = '\'';
-        else if (str[i] == '\"' && !quote)
-            quote = '\"';
-        else if (str[i] == quote)
-            quote = '\0';
-        else
-            new_string[ft_strlen(new_string)] = str[i];
-        i++;
-    }
-    return (new_string);
+		if (str[i] == '\'' && !quote)
+			quote = '\'';
+		else if (str[i] == '\"' && !quote)
+			quote = '\"';
+		else if (str[i] == quote)
+			quote = '\0';
+		else
+			new_string[ft_strlen(new_string)] = str[i];
+		i++;
+	}
+	return (new_string);
 }
 
-int check_if_quotes(char *str)
+int	check_if_quotes(char *str)
 {
-	int i;
-	int double_quote;
-	int single_quote;
+	int	i;
+	int	double_quote;
+	int	single_quote;
 
 	i = 0;
 	double_quote = 0;
 	single_quote = 0;
-	while(str[i])
+	while (str[i])
 	{
 		if (str[i] == '\'' && double_quote == 0)
 			single_quote++;
@@ -255,17 +255,17 @@ int check_if_quotes(char *str)
 			double_quote++;
 		i++;
 	}
-	if(single_quote == 0 && double_quote > 0)
+	if (single_quote == 0 && double_quote > 0)
 	{
-		if(double_quote % 2 != 0)
+		if (double_quote % 2 != 0)
 			exit(printf("wrong amount of quotes")); //handle error
-		return(1);
+		return (1);
 	}
-	else if(double_quote == 0 && single_quote > 0)
+	else if (double_quote == 0 && single_quote > 0)
 	{
-		if(single_quote % 2 != 0)
+		if (single_quote % 2 != 0)
 			exit(printf("wrong amount of quotes")); //handle error
-		return(1);
+		return (1);
 	}
 	return (0);
 }
@@ -275,26 +275,26 @@ void	open_quotes(t_list *current)
 	int	i;
 
 	i = 0;
-	if(check_if_quotes(current->value))
+	if (check_if_quotes(current->value))
 		current->value = parse_quotes(current->value);
-	while(i < current->argc)
+	while (i < current->argc)
 	{
-		if(check_if_quotes(current->args[i]))
+		if (check_if_quotes(current->args[i]))
 			current->args[i] = parse_quotes(current->args[i]);
 		i++;
 	}
 }
 
-void gatekeeper(t_list *head)
+void	gatekeeper(t_list *head)
 {
-	t_list *current;
+	t_list	*current;
 
 	current = head;
-	while(current)
+	while (current)
 	{
-		if(current->value)
+		if (current->value)
 			check_value_for_dollar(current);
-		if(current->args)
+		if (current->args)
 			check_args_for_dollar(current);
 		open_quotes(current);
 		current = current->next;
