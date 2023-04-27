041861963789162ca6434b074bb2b7140de88105
tikanoja <107203742+tikanoja@users.noreply.github.com>
2023-04-27 16:17:54 +0300

    added fd handling

---
+++ fds_and_pipes.c
@@ -0,0 +1,52 @@
+#include "minishell.h"
+
+t_list    *handle_redirection_out(t_list *current)
+{
+    t_list *prev;
+    t_list *next;
+    int fd;
+
+    prev = current->prev;
+    next = current->next;
+    if (!next || !next->value || ft_strlen(next->value) == 0)
+        printf("file not found / syntax error\n");
+    else
+    {
+        fd = open(next->value, O_WRONLY | O_CREAT, 0644);
+        if (fd == -1)
+            printf("error opening file %s\n", next->value);
+        prev->output = fd;
+    }
+    if (next && next->next)
+    {
+        prev->next = next->next;
+        free(current->value);
+        free(next->value);
+        free(current);
+        free(next);
+        return (next->next);
+    }
+    prev->next = NULL;
+    return (prev);
+}
+
+void    open_fds_and_pipes(t_list *head)
+{
+    t_list *current;
+
+    current = head;
+    while (current)
+    {
+        // if (ft_strncmp(current->value, "|\0", 1) == 0) 
+		//     return (1);
+	    // else if (ft_strncmp(current->value, "<<\0", 2) == 0)
+		//     return (2);
+	    // else if (ft_strncmp(current->value, ">>\0", 2) == 0)
+		//     return (2);
+	    // else if (ft_strncmp(current->value, "<\0", 1) == 0)
+		//     return (1);
+	    if (ft_strncmp(current->value, ">\0", 1) == 0)
+		    current = handle_redirection_out(current);
+        current = current->next;
+    }
+}
