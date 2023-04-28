431a9c51c25c77253955fc89da5b9f02274f5b9d
tikanoja <107203742+tikanoja@users.noreply.github.com>
2023-04-28 11:18:38 +0300

    redir in and append work i guess heh

--- fds_and_pipes.c
+++ fds_and_pipes.c
@@ -41,6 +41,62 @@ t_list    *handle_redirection_out(t_list *current)
     return (prev);
 }
 
+t_list    *handle_redirection_in(t_list *current)
+{
+    t_list *prev;
+    t_list *next;
+    int fd;
+
+    prev = current->prev;
+    next = current->next;
+    if (!next || !next->value || ft_strlen(next->value) == 0)
+        printf("file not found / syntax error\n"); //jos tää ni sitä cmd ei execute lainkaan!!
+    else
+    {
+        fd = open(next->value, O_RDONLY, 0644);
+        if (fd == -1)
+            printf("error opening file %s\n", next->value);
+        prev->input = fd;
+    }
+    if (next->next)
+    {
+        prev->next = next->next;
+        next->next->prev = prev;
+    }
+    else
+        prev->next = NULL;
+    free_redirection_out(current);
+    return (prev);
+}
+
+t_list    *handle_redirection_out_append(t_list *current)
+{
+    t_list *prev;
+    t_list *next;
+    int fd;
+
+    prev = current->prev;
+    next = current->next;
+    if (!next || !next->value || ft_strlen(next->value) == 0)
+        printf("file not found / syntax error\n");
+    else
+    {
+        fd = open(next->value, O_WRONLY | O_CREAT | O_APPEND, 0644);
+        if (fd == -1)
+            printf("error opening file %s\n", next->value);
+        prev->output = fd;
+    }
+    if (next->next)
+    {
+        prev->next = next->next;
+        next->next->prev = prev;
+    }
+    else
+        prev->next = NULL;
+    free_redirection_out(current);
+    return (prev);
+}
+
 void    open_fds_and_pipes(t_list *head)
 {
     t_list *current;
@@ -52,11 +108,11 @@ void    open_fds_and_pipes(t_list *head)
 		//     return (1);
 	    // else if (ft_strncmp(current->value, "<<\0", 2) == 0)
 		//     return (2);
-	    // else if (ft_strncmp(current->value, ">>\0", 2) == 0)
-		//     return (2);
-	    // else if (ft_strncmp(current->value, "<\0", 1) == 0)
-		//     return (1);
-	    if (ft_strncmp(current->value, ">\0", 1) == 0)
+	    if (ft_strncmp(current->value, ">>\0", 2) == 0)
+		    current = handle_redirection_out_append(current);
+	    else if (ft_strncmp(current->value, "<\0", 1) == 0)
+		    current = handle_redirection_in(current);
+	    else if (ft_strncmp(current->value, ">\0", 1) == 0)
 		    current = handle_redirection_out(current);
         current = current->next;
     }
