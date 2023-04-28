2ca86c75cee34c6b49167d009a7db850559cab12
tikanoja <107203742+tikanoja@users.noreply.github.com>
2023-04-28 12:29:12 +0300

    added pipes to redirection handling

--- fds_and_pipes.c
+++ fds_and_pipes.c
@@ -97,6 +97,27 @@ t_list    *handle_redirection_out_append(t_list *current)
     return (prev);
 }
 
+t_list    *handle_pipe(t_list *current)
+{
+    t_list *prev;
+    t_list *next;
+    int pipefd[2];
+
+    prev = current->prev;
+    next = current->next;
+    if (!next || !next->value || ft_strlen(next->value) == 0)
+        printf("nothing will read the pipe :(\n");
+    if (pipe(pipefd) == -1)
+        printf("error opening pipe\n");
+    prev->output = pipefd[1];
+    next->input = pipefd[0];
+    prev->next = next;
+    next->prev = prev;
+    free(current->value);
+    free(current);
+    return (prev);
+}
+
 void    open_fds_and_pipes(t_list *head)
 {
     t_list *current;
@@ -104,11 +125,11 @@ void    open_fds_and_pipes(t_list *head)
     current = head;
     while (current)
     {
-        // if (ft_strncmp(current->value, "|\0", 1) == 0) 
-		//     return (1);
+        if (ft_strncmp(current->value, "|\0", 1) == 0) 
+		    current = handle_pipe(current);
 	    // else if (ft_strncmp(current->value, "<<\0", 2) == 0)
 		//     return (2);
-	    if (ft_strncmp(current->value, ">>\0", 2) == 0)
+	    else if (ft_strncmp(current->value, ">>\0", 2) == 0)
 		    current = handle_redirection_out_append(current);
 	    else if (ft_strncmp(current->value, "<\0", 1) == 0)
 		    current = handle_redirection_in(current);
