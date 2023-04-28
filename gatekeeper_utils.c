f77792567bea109fe6b5be5d0b30c6d77d1e6800
Joonatan Aurasmaa <jaurasma@c2r3p9.hive.fi>
2023-04-28 15:43:31 +0300

    message

---
+++ gatekeeper_utils.c
@@ -0,0 +1,61 @@
+#include "minishell.h"
+
+char *ft_strcpy(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+char *char_join(char *string, char c)
+{
+    size_t len = ft_strlen(string);
+    char *new_str = ft_calloc(len + 2, sizeof(char));
+    if (!new_str) {
+        // handle allocation error
+        exit(1);
+    }
+    ft_strcpy(new_str, string);
+    new_str[len] = c;
+    new_str[len + 1] = '\0';
+    return new_str;
+}
+
+size_t	ft_strnlen(const char *s, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] != '\0' && i <= n)
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strndup(const char *string, size_t n)
+{
+	char		*dup;
+	size_t		len;
+	size_t		i;
+
+	i = 0;
+	len = ft_strnlen((char *)string, n);
+	dup = ft_calloc(len + 1, sizeof(char));
+	if (dup == NULL)
+		return (NULL);
+	while (i < len || i <=n)
+	{
+		dup[i] = string[i];
+		i++;
+	}
+	dup[i] = '\0';
+	return (dup);
+}
