d9132e90951b5ba529eda30afe10adbc25b9577d
Joonatan Aurasmaa <jaurasma@c2r3p13.hive.fi>
2023-05-22 19:44:33 +0300

    hello

--- exit.c
+++ exit.c
@@ -15,71 +15,58 @@ int check_for_non_numeric(char *str)
 	return (0);
 }
 
-// static int	toolong(int sign)
-// {
-// 	printf("EN VITTU TAOAHTU\n");
-// 	if (sign == 1)
-// 		return (-1);
-// 	else
-// 		return (0);
-// }
+int		ft_ctoi(char c)
+{
+	return(c - 42);
+}
 
-// long	ft_atol(const char *str)
-// {
-// 	long long	i;
-// 	int			sign;
+int		check_for_too_long(char *str)
+{
+	int i;
+	int j;
+	char *long_max = "9223372036854775807";
 
-// 	i = 0;
-// 	sign = 1;
-// 	while (*str == 32 || (*str >= 9 && *str <= 13))
-// 		str++;
-// 	if (*str == '-' || *str == '+')
-// 	{
-// 		if (*str == '-')
-// 			sign = sign * -1;
-// 		str++;
-// 	}
-// 	while (*str >= '0' && *str <= '9')
-// 	{
-// 		i = i * 10 + *str - '0';
-// 		printf("%lld\n", i);
-// 		if (i > 9223372036854775807)
-// 			return (toolong(sign));
-// 		str++;
-// 	}
-// 	return (i * sign);
-// }
-// int		check_for_too_long(char *str)
-// {
-// 	int i;
+	i = 0;
+	j = 0;
+	if (str[i] == '-')
+		i++;
+	if (ft_strlen(str) > ft_strlen(long_max) + i)
+		return (1);
+	else if (ft_strlen(str) < ft_strlen(long_max) + i)
+		return (0);
+	else if (ft_strlen(str) == ft_strlen(long_max) + i)
+	{
+		while (long_max[j])
+		{
+			if(ft_ctoi(str[i]) > ft_ctoi(long_max[j]))
+				return(1);
+			i++;
+			j++;
+		}
+	}
+	if (i > j)
+		if (ft_ctoi(str[i]) > 8)
+				return (1);	
+	return (0);
+}
 
-// 	i = 0;
-// 	if(ft_strlen(str) > 19)
-// 		return(1)
-// 	if(ft_strlen(str) == 19 && str[0] != '9')
-// 		return (0);
-// 	if(ft_strlen(str) == 19 && str[0] == '9')
-// 		return (0);	
-
-// 	return (0);
-// }
 void ft_exit(t_list *exit_arg, int pid)
 {
 	int exit_status;
 
 	exit_status = 0;
-	if (exit_arg->args && check_for_non_numeric(exit_arg->args[0]) == 1\
-	&& exit_arg->argc >= 1) //|| check_for_too_long(exit_arg->args[0]) == 1)
+	if ((exit_arg->args && check_for_non_numeric(exit_arg->args[0]) == 1\
+	&& exit_arg->argc >= 1) || check_for_too_long(exit_arg->args[0]) == 1)
 	{
 		printf("minishell: exit: %s: numeric argument required\n", exit_arg->args[0]);
 		exit_status = 255;
 	}
-	else if(exit_arg->argc > 1)
+	else if (exit_arg->argc > 1)
 	{
 		printf("minishell: exit: too many arguments\n");
 		return ;
 	}
-	else if(pid != 0)
+	else if (pid != 0)
 		printf("exit\n");
 	if (exit_arg->argc < 1)
 		exit (0);
