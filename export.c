58a72dc4474fb092c1b2a28ed7b7a7c852f8a5c9
Joonatan Aurasmaa <jaurasma@c2r3p13.hive.fi>
2023-05-22 19:49:45 +0300

    export norm fixed

--- export.c
+++ export.c
@@ -1,28 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   export.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaurasma <jaurasma@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/22 19:47:52 by jaurasma          #+#    #+#             */
+/*   Updated: 2023/05/22 19:49:11 by jaurasma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
-    int ft_export(t_list *current)
-    {
-        int i;
-        int status;
+int	ft_export(t_list *current)
+{
+	int	i;
+	int	status;
 
-        i = 0;
-        status = 0;
-        if (current->argc == 0)
-        {
-            while(envcpy[i] && current->argc == 0 && envcpy[i+1] != NULL)
-            {
-                printf("declare -x %s\n", envcpy[i]);
-                i++;
-            }
-            return (0);
-        }
-        while(current->argc > i)
-        {
-            status = ft_setenv(current->args[i]);
-            if (status == 1)
-                break;
-            i++;
-        }
-        return (status);
-    }   
-    //now work please yes now you are working  
+	i = 0;
+	status = 0;
+	if (current->argc == 0)
+	{
+		while (envcpy[i] && current->argc == 0 && envcpy[i + 1] != NULL)
+		{
+			printf("declare -x %s\n", envcpy[i]);
+			i++;
+		}
+		return (0);
+	}
+	while (current->argc > i)
+	{
+		status = ft_setenv(current->args[i]);
+		if (status == 1)
+			break ;
+		i++;
+	}
+	return (status);
+}
