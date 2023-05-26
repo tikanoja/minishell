b5370118717acdb2d59c8d6e4f7db75d54249a14
Joonatan Aurasmaa <jaurasma@c2r3p13.hive.fi>
2023-05-26 19:31:23 +0300

    norm norm norm norm-no-no-normo norm-no-no-norm

--- exit.c
+++ exit.c
@@ -1,33 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exit.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaurasma <jaurasma@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/26 18:58:16 by jaurasma          #+#    #+#             */
+/*   Updated: 2023/05/26 19:29:07 by jaurasma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
-int check_for_non_numeric(char *str)
+
+int	check_for_non_numeric(char *str)
 {
-	int i;
+	int	i;
 
 	i = 0;
-	if(str[i] == '-' || str[i] == '+')
+	if (str[i] == '-' || str[i] == '+')
 		i++;
-	while(str[i])
+	while (str[i])
 	{
-		if(str[i] < '0' || str[i] > '9')
+		if (str[i] < '0' || str[i] > '9')
 			return (1);
 		i++;
 	}
 	return (0);
 }
 
-int		ft_ctoi(char c)
+int	ft_ctoi(char c)
 {
-	return(c - 42);
+	return (c - 42);
 }
 
-int		check_for_too_long(char *str)
+int	check_for_too_long(char *str, int i, int j)
 {
-	int i;
-	int j;
-	char *long_max = "9223372036854775807";
+	char	*long_max;
 
-	i = 0;
-	j = 0;
+	long_max = "9223372036854775807";
 	if (str[i] == '-' || str[i] == '+')
 		i++;
 	if (ft_strlen(str) > ft_strlen(long_max) + i)
@@ -38,36 +48,39 @@ int		check_for_too_long(char *str)
 	{
 		while (long_max[j])
 		{
-			if(ft_ctoi(str[i]) > ft_ctoi(long_max[j]))
-				return(1);
-			i++;
+			if (ft_ctoi(str[i++]) > ft_ctoi(long_max[j]))
+				return (1);
 			j++;
 		}
 	}
-	if (i > j)
-		if (ft_ctoi(str[i]) > 8)
-				return (1);	
 	return (0);
+}
+
+void	print_exit_error(char *str)
+{
+	ft_putstr_fd("minishell: exit: ", 2);
+	ft_putstr_fd(str, 2);
+	ft_putstr_fd(": numeric argument required\n", 2);
 }
 
 int	ft_exit(t_list *exit_arg, int pid)
 {
-	int exit_status;
-	int flag;
+	int	exit_status;
+	int	flag;
 
 	exit_status = 0;
 	flag = 0;
-	if ((exit_arg->args && check_for_non_numeric(exit_arg->args[0]) == 1\
-	&& exit_arg->argc >= 1) ||\
-	(exit_arg->args && check_for_too_long(exit_arg->args[0]) == 1))
+	if ((exit_arg->args && check_for_non_numeric(exit_arg->args[0]) == 1 \
+	&& exit_arg->argc >= 1) || \
+	(exit_arg->args && check_for_too_long(exit_arg->args[0], 0, 0) == 1))
 	{
-		printf("minishell: exit: %s: numeric argument required\n", exit_arg->args[0]);
+		print_exit_error(exit_arg->args[0]);
 		exit_status = 255;
 		flag = 1;
 	}
 	else if (exit_arg->argc > 1)
 	{
-		printf("minishell: exit: too many arguments\n");
+		ft_putstr_fd("minishell: exit: too many arguments\n", 2);
 		return (1);
 	}
 	else if (pid != 0)
