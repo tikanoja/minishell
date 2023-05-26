330dd1ff4a468e6a94f48af53fa094ee22ad7377
Joonatan Aurasmaa <jaurasma@c2r3p13.hive.fi>
2023-05-26 18:19:24 +0300

    norm norm norm norm

---
+++ check_value_for_dollar.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_value_for_dollar.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaurasma <jaurasma@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/26 18:08:28 by jaurasma          #+#    #+#             */
+/*   Updated: 2023/05/26 18:14:32 by jaurasma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	check_value_for_dollar(t_list *current, int status)
+{
+	char	*new_value;
+
+    printf("here\n");
+	if (check_for_dollar(current->value) && \
+	check_for_quote_dollar(current->value))
+	{
+		new_value = process_arg(current->value, status, \
+		current, ft_strlen(current->value));
+        free(current->value);
+		if (*new_value == '\0')
+			new_value = NULL;
+		current->value = new_value;
+		if (current->value == NULL)
+			current->execflag = 1;
+	}
+}
