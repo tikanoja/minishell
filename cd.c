98a35d0776550c9dc2337798db7bb6242401f071
Joonatan Aurasmaa <jaurasma@c2r3p13.hive.fi>
2023-05-26 18:41:15 +0300

    Yo, listen up, here's the story About a little guy that lives in a norm world And all day and all night and everything he sees is just norm Like him, inside and outside Norm his house with a norm little window And a norm Corvette and everything is norm for him And himself and everybody around 'Cause he ain't got nobody to listen (To listen, to listen, to listen)
    [Chorus]
    I'm norm, da ba dee da ba di
    Da ba dee da ba di, da ba dee da ba di
    Da ba dee da ba di, da ba dee da ba di
    Da ba dee da ba di, da ba dee da ba di
    I'm norm, da ba dee da ba di
    Da ba dee da ba di, da ba dee da ba di
    Da ba dee da ba di, da ba dee da ba di
    Da ba dee da ba di, da ba dee da ba di
    
    [Verse]
    I have a norm house with a norm window
    Norm is the color of all that I wear
    Norm are the streets and all the trees are too
    I have a girlfriend and she is so norm
    Norm are the people here that walk around
    Norm like my Corvette, it's standing outside
    Norm are the words I say and what I think
    Norm are the feelings that live inside me
    
    I'm norm, da ba dee da ba di
    Da ba dee da ba di, da ba dee da ba di
    Da ba dee da ba di, da ba dee da ba di
    Da ba dee da ba di, da ba dee da ba di
    I'm norm, da ba dee da ba di
    Da ba dee da ba di, da ba dee da ba di
    Da ba dee da ba di, da ba dee da ba di
    Da ba dee da ba di, da ba dee da ba di
    
    I have a norm house with a norm window
    Norm is the color of all that I wear
    Norm are the streets and all the trees are too
    I have a girlfriend and she is so norm.
    Norm are the people here that walk around
    Norm like my Corvette, it's standing outside
    Norm are the words I say and what I think
    Norm are the feelings that live inside me
    
    I'm norm, da ba dee da ba di
    Da ba dee da ba di, da ba dee da ba di
    Da ba dee da ba di, da ba dee da ba di
    Da ba dee da ba di, da ba dee da ba di
    I'm norm, da ba dee da ba di
    Da ba dee da ba di, da ba dee da ba di
    Da ba dee da ba di, da ba dee da ba di
    Da ba dee da ba di, da ba dee da ba di
    
    Inside and outside
    Norminette his house with a norm little window
    And a norm Corvette and everything is norm for him
    And himself and everybody around
    'Cause he ain't got nobody to listen (To listen)
    
    I'm norm, da ba dee da ba di
    Da ba dee da ba di, da ba dee da ba di
    Da ba dee da ba di, da ba dee da ba di
    Da ba dee da ba di, da ba dee da ba di
    I'm norm, da ba dee da ba di
    Da ba dee da ba di, da ba dee da ba di
    Da ba dee da ba di, da ba dee da ba di
    Da ba dee da ba di, da ba dee da ba di

--- cd.c
+++ cd.c
@@ -1,11 +1,21 @@
-#include "minishell.h"
-#include <limits.h> //tän vois vissiin vaan definaa PATH_MAX 1024?
-#include <errno.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cd.c                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaurasma <jaurasma@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/26 18:30:21 by jaurasma          #+#    #+#             */
+/*   Updated: 2023/05/26 18:34:31 by jaurasma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-char *ft_strcat(char *dest, const char *src)
+#include "minishell.h"
+
+char	*ft_strcat(char *dest, const char *src)
 {
-	char *result; 
-	
+	char	*result;
+
 	result = dest;
 	while (*dest != '\0')
 		dest++;
@@ -15,13 +25,12 @@ char *ft_strcat(char *dest, const char *src)
 	return (result);
 }
 
-int cd_home(void)
+int	cd_home(void)
 {
-	char *pwd;
-	char *old_pwd;
-	char cwd[PATH_MAX];
-	
-	// old_pwd = getcwd(cwd, PATH_MAX);
+	char	*pwd;
+	char	*old_pwd;
+	char	cwd[PATH_MAX];
+
 	old_pwd = ft_strjoin("OLDPWD=", getcwd(cwd, PATH_MAX));
 	if (chdir(ft_getenv("HOME")) == -1)
 	{
@@ -35,17 +44,16 @@ int cd_home(void)
 	ft_setenv(pwd);
 	free(pwd);
 	free(old_pwd);
-	return(0);
+	return (0);
 }
 
-int cd_back(void)
+int	cd_back(void)
 {
-	char *pwd;
-	char *old_pwd;
-	char cwd[PATH_MAX];
-	int status;
+	char	*pwd;
+	char	*old_pwd;
+	char	cwd[PATH_MAX];
+	int		status;
 
-	//old_pwd = getcwd(cwd, PATH_MAX);
 	old_pwd = ft_strjoin("OLDPWD=", getcwd(cwd, PATH_MAX));
 	status = 0;
 	if (chdir(ft_getenv("OLDPWD")) == -1)
@@ -56,7 +64,6 @@ int cd_back(void)
 		status = errno;
 		return (status);
 	}
-	// pwd = getcwd(cwd, PATH_MAX);
 	pwd = ft_strjoin("PWD=", getcwd(cwd, PATH_MAX));
 	ft_setenv(old_pwd);
 	ft_setenv(pwd);
@@ -65,27 +72,25 @@ int cd_back(void)
 	return (status);
 }
 
-int cd_path(char *path, t_list *current)
+int	cd_path(char *path, t_list *current)
 {
-	char *pwd;
-	char *old_pwd;
-	char cwd[PATH_MAX];
-	int status;
+	char	*pwd;
+	char	*old_pwd;
+	char	cwd[PATH_MAX];
+	int		status;
 
-	//old_pwd = getcwd(cwd, PATH_MAX);
 	old_pwd = ft_strjoin("OLDPWD=", getcwd(cwd, PATH_MAX));
 	status = 0;
 	if (chdir(path) == -1)
-    {
-        ft_putstr_fd("shelly: cd: ", STDERR_FILENO);
-        ft_putstr_fd(current->args[0], STDERR_FILENO);
-        ft_putstr_fd(": ", STDERR_FILENO);
-        perror(""); // Print the error message corresponding to errno
-        free(old_pwd);
-        status = errno; // Set the error status to the value of errno
+	{
+		ft_putstr_fd("shelly: cd: ", STDERR_FILENO);
+		ft_putstr_fd(current->args[0], STDERR_FILENO);
+		ft_putstr_fd(": ", STDERR_FILENO);
+		perror("");
+		free(old_pwd);
+		status = errno;
 		return (status);
-    }
-	// pwd = getcwd(cwd, PATH_MAX);
+	}
 	pwd = ft_strjoin("PWD=", getcwd(cwd, PATH_MAX));
 	ft_setenv(old_pwd);
 	ft_setenv(pwd);
@@ -94,18 +99,18 @@ int cd_path(char *path, t_list *current)
 	return (status);
 }
 
-int ft_cd(t_list *current) //vitusti lisaa error management pitaako ottaa real path kayttoon
+int	ft_cd(t_list *current)
 {
-	char *path;
-	int	status;
-	
+	char	*path;
+	int		status;
+
 	path = NULL;
 	status = 0;
-	if(current->argc > 0)
+	if (current->argc > 0)
 		path = ft_strdup(current->args[0]);
 	if (current->argc == 0 || (path[0] == '~' && path[1] == '\0'))
 		status = cd_home();
-	else if((path[0] == '-' && path[1] == '\0'))
+	else if ((path[0] == '-' && path[1] == '\0'))
 		status = cd_back();
 	else
 		status = cd_path(path, current);
