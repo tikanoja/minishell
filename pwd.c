7fa1865b6e27a6e881397d51db14b56451bf0a5b
Joonatan Aurasmaa <jaurasma@c2r3p13.hive.fi>
2023-05-26 15:23:22 +0300

    pwd norm  ok

--- pwd.c
+++ pwd.c
@@ -1,10 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pwd.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaurasma <jaurasma@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/26 15:21:46 by jaurasma          #+#    #+#             */
+/*   Updated: 2023/05/26 15:23:07 by jaurasma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
-int ft_pwd(void)
+int	ft_pwd(void)
 {
-	char cwd[1024];
+	char	cwd[1024];
+
 	if (getcwd(cwd, sizeof(cwd)) != NULL)
-		printf("%s\n", cwd); 
+		printf("%s\n", cwd);
 	else
 	{
 		perror("getcwd() error");
