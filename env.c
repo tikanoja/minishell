b8b5d7541f58ec64a1c76ae16b6517248cb6bef5
Joonatan Aurasmaa <jaurasma@c2r3p13.hive.fi>
2023-05-27 15:48:04 +0300

    im env-ious of your norm

--- env.c
+++ env.c
@@ -6,7 +6,7 @@
 /*   By: jaurasma <jaurasma@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/27 14:50:27 by jaurasma          #+#    #+#             */
-/*   Updated: 2023/05/27 15:40:46 by jaurasma         ###   ########.fr       */
+/*   Updated: 2023/05/27 15:46:27 by jaurasma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 char	*ft_getenv(const char *name)
 {
-	char **env;
-	size_t namelen;
-	char *equals;
+	char	**env;
+	size_t	namelen;
+	char	*equals;
 
 	env = envcpy;
 	namelen = 0;
@@ -34,11 +34,8 @@ char	*ft_getenv(const char *name)
 	return (NULL);
 }
 
-int ft_env(t_list *current)
+int	ft_env(t_list *current, int i)
 {
-	int i;
-
-	i = 0;
 	if (current->args)
 	{
 		if (access(current->args[0], F_OK) == 0)
@@ -46,19 +43,19 @@ int ft_env(t_list *current)
 			ft_putstr_fd("env: ", STDERR_FILENO);
 			ft_putstr_fd(current->args[0], STDERR_FILENO);
 			ft_putstr_fd(": Premission denied\n", STDERR_FILENO);
-			return (126);          
+			return (126);
 		}
 		else
 		{
 			ft_putstr_fd("env: ", STDERR_FILENO);
 			ft_putstr_fd(current->args[0], STDERR_FILENO);
 			ft_putstr_fd(": No such file or directory\n", STDERR_FILENO);
-			return (127);               
+			return (127);
 		}
 	}
-	while(envcpy[i])
+	while (envcpy[i])
 	{
-		if(ft_strchr(envcpy[i], '='))
+		if (ft_strchr(envcpy[i], '='))
 			printf("%s\n", envcpy[i]);
 		i++;
 	}
@@ -67,7 +64,7 @@ int ft_env(t_list *current)
 
 int	ft_strcmp(char *s1, char *s2)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0')
