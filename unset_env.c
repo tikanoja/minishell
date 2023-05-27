2b04966c02cc4dc2e206cfe86263f7b45a29ec79
Joonatan Aurasmaa <jaurasma@c2r3p13.hive.fi>
2023-05-27 15:41:49 +0300

    this is norm-al

---
+++ unset_env.c
@@ -0,0 +1,103 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   unset_env.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaurasma <jaurasma@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/27 14:52:54 by jaurasma          #+#    #+#             */
+/*   Updated: 2023/05/27 15:38:43 by jaurasma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	check_for_valid_key_unset(char *key, int *status, int *flag, int len)
+{
+	int	index;
+
+	index = 0;
+	while (index < len)
+	{
+		if (check_key_chars(key[index], index) == 0)
+		{
+			ft_putstr_fd("'", STDERR_FILENO);
+			ft_putstr_fd(key, STDERR_FILENO);
+			ft_putstr_fd("' : not a valid identifier\n", STDERR_FILENO);
+			(*status) = 1;
+			(*flag) = 1;
+			break ;
+		}
+		index++;
+	}
+}
+
+int	find_key_index(char *key, int *i, int flag, int len)
+{
+	int	index;
+
+	index = -1;
+	while (envcpy[(*i)] && flag == 0)
+	{
+		if (ft_strncmp(envcpy[(*i)], key, len - 1) == 0 && \
+		(envcpy[(*i)][len] == '=' || envcpy[(*i)][len] == '\0'))
+			index = (*i);
+		(*i)++;
+	}
+	return (index);
+}
+
+void	unset_envcpy(t_list *current, int index, char *key, int i)
+{
+	char	**new_env;
+	int		j;
+
+	new_env = ft_calloc(i, sizeof(char *));
+	i = 0;
+	j = 0;
+	if (new_env == NULL)
+		exit_gracefully(current);
+	while (envcpy[i])
+	{
+		if (i != index)
+		{
+			new_env[j] = ft_strdup(envcpy[i]);
+			if (new_env[j] == NULL)
+				exit_gracefully(current);
+			j++;
+		}
+		free(envcpy[i]);
+		i++;
+	}
+	new_env[j] = NULL;
+	free(envcpy);
+	envcpy = NULL;
+	envcpy = new_env;
+	free(key);
+}
+
+int	ft_unsetenv(t_list *current, int k, int len, int status)
+{
+	int		index;
+	int		i;
+	char	*key;
+	int		flag;
+
+	while (current->args && current->args[k])
+	{
+		flag = 0;
+		key = ft_strdup(current->args[k]);
+		if (key == NULL)
+			exit_gracefully(current);
+		len = ft_strlen(key);
+		i = 0;
+		check_for_valid_key_unset(key, &status, &flag, len);
+		index = find_key_index(key, &i, flag, len);
+		if (index == -1)
+			free(key);
+		else if (flag == 0)
+			unset_envcpy(current, index, key, i);
+		k++;
+	}
+	return (status);
+}
