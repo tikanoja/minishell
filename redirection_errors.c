b4304e6dd1bd6defe3d07e03e0a75d7b9c800376
Tuukka Tikanoja <ttikanoj@c2r3p11.hive.fi>
2023-05-29 13:51:29 +0300

    redr errs file

---
+++ redirection_errors.c
@@ -0,0 +1,57 @@
+#include "minishell.h"
+
+void	redir_directory_check_prints(char *str, int flag)
+{
+	ft_putstr_fd("shelly: ", 2);
+	ft_putstr_fd(str, 2);
+	if (flag == 1)
+		ft_putstr_fd(": is a directory\n", 2);
+	else if (flag == 2)
+		ft_putstr_fd(": Permission denied\n", 2);
+	else if (flag == 3)
+		ft_putstr_fd(": No such file or directory\n", 2);
+}
+
+void	redir_out_null_next(t_list *prev)
+{
+	write(2, "shelly: syntax error near unexpected token 'newline'\n", 53);
+	if (prev)
+		prev->execflag = 1;
+}
+
+void	redir_out_double_redir(t_list *prev, t_list *next)
+{
+	ft_putstr_fd("shelly: syntax error near unexpected token '", 2);
+	ft_putstr_fd(next->value, 2);
+	ft_putstr_fd("'\n", 2);
+	if (prev)
+		prev->execflag = 1;
+}
+
+int redir_directory_check(char *str, t_list *prev)
+{
+	struct stat file_info;
+
+	if (stat(str, &file_info) == 0)//protect?
+	{
+		if (S_ISDIR(file_info.st_mode))
+		{
+			if (prev == NULL || (prev && prev->execflag != 1))
+				redir_directory_check_prints(str, 1);
+			return (1);
+		}
+		else if(S_ISREG(file_info.st_mode) && access(str, X_OK) != 0)
+		{
+			if (prev == NULL || (prev && prev->execflag != 1))
+				redir_directory_check_prints(str, 2);
+			return (1);
+		}
+	}
+	else
+	{
+		if (prev == NULL || (prev && prev->execflag != 1))
+			redir_directory_check_prints(str, 3);
+		return (1);
+	}
+	return (0);
+}
