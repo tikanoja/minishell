3a1429b44f74518088ddb0bef84602efc8a088b4
Tuukka Tikanoja <ttikanoj@c2r4p11.hive.fi>
2023-05-29 22:44:39 +0300

    redir error messages

--- errors.c
+++ errors.c
@@ -157,39 +157,35 @@ int	is_it_redirection_redir_check(char *prompt)
 	return (0);
 }
 
+int	double_redir_print(int flag, t_list *current, t_list *head)
+{
+	ft_putstr_fd("shelly: syntax error near unexpected token `", 2);
+	if (flag == 1)
+		ft_putstr_fd(current->value, 2);
+	else if (flag == 2)
+		ft_putstr_fd(current->next->value, 2);
+	else if (flag == 3)
+		ft_putstr_fd(current->args[0], 2);
+	ft_putstr_fd("'\n", 2);
+	free_list(head);
+	return (1);
+}
+
 int	double_redir_check(t_list *head)
 {
 	t_list	*current;
 
 	current = head;
 	if (ft_strncmp(current->value, "|", 1) == 0)
-	{
-		ft_putstr_fd("shelly: syntax error near unexpected token `", 2);
-		ft_putstr_fd(current->value, 2);
-		ft_putstr_fd("'\n", 2);
-		free_list(head);
-		return (1);
-	}
+		return (double_redir_print(1, current, head));
 	while (current)
 	{
 		if (is_it_redirection_no_pipe(current->value) > 0 && \
 		current->next && is_it_redirection_no_pipe(current->next->value) > 0)
-		{
-			ft_putstr_fd("shelly: syntax error near unexpected token `", 2);
-			ft_putstr_fd(current->next->value, 2);
-			ft_putstr_fd("'\n", 2);
-			free_list(head);
-			return (1);
-		}
+			return (double_redir_print(2, current, head));
 		else if (current->args && \
 		is_it_redirection_redir_check(current->args[0]) > 0)
-		{
-			ft_putstr_fd("shelly: syntax error near unexpected token `", 2);
-			ft_putstr_fd(current->args[0], 2);
-			ft_putstr_fd("'\n", 2);
-			free_list(head);
-			return (1);
-		}
+			return (double_redir_print(3, current, head));
 		current = current->next;
 	}
 	return (0);
