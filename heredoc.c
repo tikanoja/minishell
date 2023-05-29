15cbb5a87ef2530374a1ad3e530977e2fe546600
Joonatan Aurasmaa <jaurasma@c2r3p13.hive.fi>
2023-05-29 20:57:46 +0300

    here norm

--- heredoc.c
+++ heredoc.c
@@ -1,21 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   heredoc.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaurasma <jaurasma@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/29 20:56:00 by jaurasma          #+#    #+#             */
+/*   Updated: 2023/05/29 20:57:27 by jaurasma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
 char	*heredoc_env_open(char *input, t_list *current)
 {
-	char    input_env[1024];
-	char    *open;
-	int     i;
-	int     j;
+	char	input_env[1024];
+	char	*open;
+	int		i;
+	int		j;
 
 	init_heredoc_env_open(&i, &j, &open, input_env);
-	while(input[i])
+	while (input[i])
 	{
-		if(input[i] == '$' && input[i + 1] != '\'' && input[i + 1] != '\"')
+		if (input[i] == '$' && input[i + 1] != '\'' && input[i + 1] != '\"')
 		{
 			i++;
-			if(input[i] != '\'' && input[i] != '\"')
+			if (input[i] != '\'' && input[i] != '\"')
 			{
-				while(is_it_whitespace(input[i]) == 0 &&\
+				while (is_it_whitespace(input[i]) == 0 && \
 				is_it_quote(input[i]) == 0 && input[i] && input[i] != '$')
 					copy_and_move_ptrs(input_env, input, &i, &j);
 				open = ft_strjoin_oe(open, ft_getenv(input_env));
@@ -26,12 +38,12 @@ char	*heredoc_env_open(char *input, t_list *current)
 			open = char_join(open, input[i], current);
 		i++;
 	}
-	return(free_heredoc_env_open(&input, open, current));
+	return (free_heredoc_env_open(&input, open, current));
 }
 
-void heredoc_loop(t_list *current, int pipefd[2], char *input, char *delim)
+void	heredoc_loop(t_list *current, int pipefd[2], char *input, char *delim)
 {
-	while(1)
+	while (1)
 	{
 		if (last_try_static_c(0) == 1)
 		{
@@ -44,15 +56,15 @@ void heredoc_loop(t_list *current, int pipefd[2], char *input, char *delim)
 		if (!input)
 		{
 			printf("\033[1A> ");
-			break;
+			break ;
 		}
-		if ((ft_strncmp(input, delim, ft_strlen(delim)) == 0 &&\
+		if ((ft_strncmp(input, delim, ft_strlen(delim)) == 0 && \
 		input[ft_strlen(delim)] == '\0'))
 		{
 			free(input);
 			break ;
 		}
-		if(check_for_dollar(input) == 1)
+		if (check_for_dollar(input) == 1)
 			input = heredoc_env_open(input, current);
 		write_input_to_pipe(input, pipefd[1]);
 	}
@@ -64,7 +76,7 @@ t_list	*handle_heredoc(t_list *current)
 	char	*input;
 	int		pipefd[2];
 	t_list	*prev;
-	
+
 	prev = current->prev;
 	input = NULL;
 	last_try_static_c(-1);
