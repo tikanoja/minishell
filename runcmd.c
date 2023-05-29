9322c66d0e4f94fe4d25bb675d12c0f98e92b3a7
Joonatan Aurasmaa <jaurasma@c2r3p13.hive.fi>
2023-05-29 22:30:13 +0300

    please work dear god Im fukcking done

--- runcmd.c
+++ runcmd.c
@@ -8,24 +8,18 @@ int     directory_check(char *str)
 	{
 		if (S_ISDIR(file_info.st_mode))
 		{
-			ft_putstr_fd("shelly: ", 2);
-			ft_putstr_fd(str, 2);
-			ft_putstr_fd(": is a directory\n", 2);
+			redir_directory_check_prints(str, 1);
 			return (1);
 		}
 		else if(S_ISREG(file_info.st_mode) && access(str, X_OK) != 0)
 		{
-			ft_putstr_fd("shelly: ", 2);
-			ft_putstr_fd(str, 2);
-			ft_putstr_fd(": Permission denied\n", 2);
+			redir_directory_check_prints(str, 2);
 			return (1);
 		}
 	}
 	else
 	{
-		ft_putstr_fd("shelly: ", 2);
-		ft_putstr_fd(str, 2);
-		ft_putstr_fd(": No such file or directory\n", 2);
+		redir_directory_check_prints(str, 3);
 		return (1);
 	}
 	return (0);
@@ -43,6 +37,14 @@ int     slash_check(char *str)
 		i++;
 	}
 	return (0);
+}
+
+void	fd_handling_close_fds(t_list *current)
+{
+	if (current->input != STDIN_FILENO)
+		close(current->input);
+	if (current->output != STDOUT_FILENO)
+		close(current->output);
 }
 
 void    fd_handling2(t_list *current, t_list *head)
@@ -77,22 +79,51 @@ void    fd_handling2(t_list *current, t_list *head)
 	}
 }
 
+void	init_execute_builtin(pid_t *pid, int *forkflag, int *status)
+{
+	(*pid) = 1;
+	(*forkflag) = 0;
+	(*status) = 0;
+}
+
+int		builtin_ladder(int status, t_list *current, pid_t pid)
+{
+	if (ft_strncmp_casein(current->value, "echo", 5) == 0)
+			status = ft_echo(current);
+	else if (ft_strncmp_casein(current->value, "pwd", 4) == 0)
+			status = ft_pwd();
+	else if (ft_strncmp_casein(current->value, "exit", 5) == 0)
+			status = ft_exit(current, pid);
+	else if (ft_strncmp_casein(current->value, "env", 5) == 0)
+			status = ft_env(current, 0);
+	else if (ft_strncmp_casein(current->value, "unset", 6) == 0)
+			status = ft_unsetenv(current, 0, 0, 0);
+	else if (ft_strncmp_casein(current->value, "export", 7) == 0)
+			status = ft_export(current);
+	else if (ft_strncmp_casein(current->value, "cd", 3) == 0)
+			status = ft_cd(current);
+	if (pid == 0)
+		exit(0);
+	return (status);
+}
+
 int    execute_builtin(t_list *current)
 {
 	pid_t pid;
 	int forkflag;
 	int status;
 
-	pid = 1; //getpid
+	pid = 1;
 	forkflag = 0;
 	status = 0;
+	init_execute_builtin(&pid, &forkflag, &status);
 	if (current->pipe == 1)
 	{
 		init_child_signals();
 		forkflag = 1;
 		pid = fork();
 		if (pid == -1)
-			exitmsg("fork fail\n");
+			exit_gracefully(current);
 		if (pid == 0)
 		{
 			fd_handling2(current, get_head_node(current));
@@ -100,24 +131,7 @@ int    execute_builtin(t_list *current)
 		}
 	}
 	if (forkflag == 0)
-	{
-		if (ft_strncmp_casein(current->value, "echo", 5) == 0)
-				status = ft_echo(current);
-		else if (ft_strncmp_casein(current->value, "pwd", 4) == 0)
-				status = ft_pwd();
-		else if (ft_strncmp_casein(current->value, "exit", 5) == 0)
-				status = ft_exit(current, pid);
-		else if (ft_strncmp_casein(current->value, "env", 5) == 0)
-				status = ft_env(current, 0);
-		else if (ft_strncmp_casein(current->value, "unset", 6) == 0)
-				status = ft_unsetenv(current, 0, 0, 0);
-		else if (ft_strncmp_casein(current->value, "export", 7) == 0)
-				status = ft_export(current);
-		else if (ft_strncmp_casein(current->value, "cd", 3) == 0)
-				status = ft_cd(current);
-		if (pid == 0)
-			exit(0);
-	}
+		status = builtin_ladder(status, current, pid);
 	return (status);
 }
 
@@ -162,35 +176,64 @@ void	cmd_not_found_update_status(t_list *current, int *status)
 	(*status) = 127;
 }
 
+void init_runcmd(t_list **current, int *status, pid_t *pid, t_list **head)
+{
+	*current = *head;
+	(*status) = 0;
+	(*pid) = 1;
+}
+
+int execflag_check(t_list **current)
+{
+	if ((*current)->execflag == 1)
+	{
+		(*current) = (*current)->next;
+		return (1);
+	}
+	return (0);
+}
+
+int runcmd_directory_check(int *status, t_list **current)
+{
+	if (directory_check((*current)->value) != 0)
+	{
+		(*current)->execflag = 1;
+		(*status) = 126;
+	}
+	else
+	{
+		(*current)->system_command = 1;
+		return (1);
+	}
+	return (0);
+}
+
+// int		end_runcmd(t_list **current, t_list **head, int status, pid_t pid)
+// {
+// 	(*current) = (*head);
+// 	close_all_fds((*current));
+// 	while (pid > 0)
+// 		pid = waitpid(-1, &status, 0);
+// 	return (status);
+// }
+
 int    runcmd(t_list *head, char **envcpy)
 {
 	t_list	*current;
 	pid_t	pid;
 	int		status;
 	
-	current = head;
-	status = 0;
+	init_runcmd(&current, &status, &pid, &head);
 	while (current)
 	{
-		if (current->execflag == 1)
-		{
-			current = current->next;
-			continue ;
-		}
+		if (execflag_check(&current) == 1)
+			continue;
 		if (current->value == NULL)
 			cmd_not_found_update_status(current, &status);
 		else if (slash_check(current->value) == 1 && current->system_command != 1)
 		{
-			if (directory_check(current->value) != 0)
-			{
-				current->execflag = 1;
-				status = 126;
-			}
-			else
-			{
-				current->system_command = 1;
-				continue;
-			}
+			if (runcmd_directory_check(&status, &current) == 1)
+				continue ;
 		}
 		else if (is_it_builtin(current->value) > 0)
 			status = execute_builtin(current);
@@ -200,6 +243,8 @@ int    runcmd(t_list *head, char **envcpy)
 	}
 	current = head;
 	close_all_fds(current);
-	while ((pid = waitpid(-1, &status, 0)) > 0);
+	//while ((pid = waitpid(-1, &status, 0)) > 0); 
+	while (pid > 0)
+		pid = waitpid(-1, &status, 0);
 	return (status);
 }
