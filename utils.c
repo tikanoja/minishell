cc50ee8bcefd44ad697237ba258406163ba51f85
Joonatan Aurasmaa <jaurasma@c2r3p13.hive.fi>
2023-05-29 20:38:33 +0300

    norm norm

--- utils.c
+++ utils.c
@@ -6,7 +6,7 @@
 /*   By: jaurasma <jaurasma@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/26 12:56:00 by jaurasma          #+#    #+#             */
-/*   Updated: 2023/05/29 17:34:21 by jaurasma         ###   ########.fr       */
+/*   Updated: 2023/05/29 20:38:13 by jaurasma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,4 +53,29 @@ void	print_error_sys_cmd(t_list *current)
 	ft_putstr_fd("shelly: ", 2);
 	ft_putstr_fd(current->value, 2);
 	ft_putstr_fd(": No such file or directory\n", 2);
+}
+
+char	*ft_strjoin_oe(char *s1, char *s2)
+{
+	char	*ns;
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	if (s2 == NULL)
+		return (s1);
+	if (s1 == NULL)
+		return (ft_strdup(s2));
+	ns = ft_calloc(ft_strlen(s1) + ft_strlen(s2) + 1, sizeof(char));
+	if (!ns)
+		return (NULL);
+	while (s1[i])
+		ns[j++] = s1[i++];
+	i = 0;
+	while (s2[i])
+		ns[j++] = s2[i++];
+	free(s1);
+	ns[j] = '\0';
+	return (ns);
 }
